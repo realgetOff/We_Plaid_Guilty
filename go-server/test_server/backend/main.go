c1222e15f7c36f5483166c3da45083c3bc8dfdd5
Nathan Michelle <namichel@3A5.42angouleme.fr>
2026-03-01 05:17:45 +0100

    Compile APP on x86 + build img (x86 -> Graviton) push on AWS IAM + pull + compose  on EC2 [skip ci]

---
+++ go-server/test_server/backend/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq" // Driver Postgres
+)
+
+func main() {
+	// 1. Récupération de la config (via env)
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", 
+		dbHost, dbUser, dbPass, dbName)
+
+	// 2. Test de connexion DB
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// 3. Simple API pour vérifier que ça répond
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		err := db.Ping()
+		if err != nil {
+			http.Error(w, "DB down", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprint(w, "Pong! Backend Go et Postgres sont copains.")
+	})
+
+	log.Println("Serveur lancé sur le port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

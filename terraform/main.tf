e7360ca7b19845fa931ee27a5a9e858a9d38d040
Nathan Michelle <82233442+Kazibuya@users.noreply.github.com>
2026-03-05 06:21:00 +0100

    add port 80 in AWS(terraform)

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,12 @@ resource "aws_security_group" "ssh_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0

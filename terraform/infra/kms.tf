e3839b8596a06d9116fac8ccd6cf03d527ca6d34
Nathan Michelle <nathan.michelleslain@gmail.com>
2026-04-22 16:27:36 +0200

    fix: checkov: CCKV2_AWS_64: Ensure KMS key Policy is defined [skip ci]

--- terraform/infra/kms.tf
+++ terraform/infra/kms.tf
@@ -3,6 +3,33 @@ resource "aws_kms_key" "vault_unseal" {
   deletion_window_in_days = 10
   enable_key_rotation     = true
   tags                    = { Name = var.project_name }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM root permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.aws_account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow K3s role to use the key"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.aws_account_id}:role/k3s-role"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_kms_alias" "vault_unseal" {
